4309af4d05940782d1667640d2eafd7f17f0e3e5
myj4513 <78058268+myj4513@users.noreply.github.com>
2022-01-24 15:25:11 +0900

    create project momo
    프로젝트 생성
    Project : Gradle Project
    Spring Boot Version : 2.6.3
    Group : com.project
    Artifact : momo
    Packaging : Jar
    Java Version : 11
    Dependencies
    - Lombok
    - Spring Data JPA
    - Spring Web
    - Spring Security
    - MySQL Driver
    - Validation

---
+++ src/main/java/com/project/momo/MomoApplication.java
@@ -0,0 +1,13 @@
+package com.project.momo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/momo/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
